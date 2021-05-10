2359b6c12ec7f45ec43dcf0f4bee7a3a5c453a01
ducgiangtrankma <tranducgiangact@gmail.com>
2021-05-10 22:06:32 +0700

    create

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text style={{fontSize: 32, fontFamily: 'DancingScript-Regular'}}>
+        Test Fonts
+      </Text>
+    </SafeAreaView>
+  );
+}
